12c2aee1ccf9597ef3137216e49a5251cf0fdc9b
mattyazzz <wideredfoxy@gmail.com>
2026-09-16 03:12:29 -0600

    cuando sera el dia q algo de lo que haga funcione?

--- leaf-chat/src/main.rs
+++ leaf-chat/src/main.rs
@@ -1,7 +1,17 @@
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
-    let app = AppWindow::new()?;
+    println!("1. arrancó");
 
-    app.run()
+    let app = AppWindow::new()?;
+    println!("2. AppWindow creado");
+
+    app.show()?;
+    println!("3. AppWindow mostrado");
+
+    slint::run_event_loop()?;
+
+    println!("4. salió del event loop");
+
+    Ok(())
 }
