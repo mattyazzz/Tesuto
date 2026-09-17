2c9e64c6818e80e54de75366662d94ca857717d4
mattyazzz <wideredfoxy@gmail.com>
2026-09-16 23:30:59 -0600

    yeh

--- leaf-chat/src/main.rs
+++ leaf-chat/src/main.rs
@@ -1,17 +1,17 @@
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
-    println!("1. arrancó");
+    println!("log 1: arrancó");
 
     let app = AppWindow::new()?;
-    println!("2. AppWindow creado");
+    println!("log 2: AppWindow creado");
 
     app.show()?;
-    println!("3. AppWindow mostrado");
+    println!("log 3: AppWindow mostrado");
 
     slint::run_event_loop()?;
 
-    println!("4. salió del event loop");
+    println!("log 4: event loop terminado");
 
     Ok(())
 }
